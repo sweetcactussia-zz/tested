9e96409bd563f0331571b14cf7753374840153a1
sweetcactus <38179931+sweetcactussia@users.noreply.github.com>
2018-04-14 15:48:34 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const chalk = require('chalk');
 module.exports = client => { // eslint-disable-line no-unused-vars
   console.log(chalk.bgGreen.black('I\'m Online'));
+  client.user.setActivity("wb!help | Alicia#4172", {type: "STREAMING"});
 };
